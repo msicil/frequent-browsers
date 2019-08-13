5309502ac7386ca5b9745772d416d61263e3a8c2
msicil <mikejoesis@gmail.com>
2019-08-12 20:42:43 -0400

    add pipenv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/michaelsiciliano/Desktop/Projects/sitevisits/venv"
+set -gx VIRTUAL_ENV "/Users/michaelsiciliano/Desktop/Projects/frequent-browsers/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
